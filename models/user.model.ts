baf3cf70ac68fca06f11050d1f134e32bb5a2303
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-05 18:44:58 -0400

    Add user model with required fields and timestamps

---
+++ models/user.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model, models } from 'mongoose'
+import type { IUser } from '@/types'
+
+const UserSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: [true, 'Email already exists'],
+    },
+
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+    },
+
+    image: {
+      type: String,
+    },
+
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Property',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// either export the existing user model or create a new user model
+export default models.UserSchema || model<IUser>('UserSchema', UserSchema)
