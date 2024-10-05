abece142eb036354919f754905414c2db29c5772
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-04 22:39:22 -0400

    Refactor properties page to display property cards with rates and property details

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,4 +1,23 @@
+import PropertyCard from '@/components/property/property-card.component'
+import properties from '@/properties.json'
+
 const PropertiesPage = () => {
-  return <div>PropertiesPage</div>
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {!properties.length ? (
+          <p>No properties available</p>
+        ) : (
+          <div className='grid gird-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((property) => {
+              const { _id } = property
+
+              return <PropertyCard key={_id} property={property} />
+            })}
+          </div>
+        )}
+      </div>
+    </section>
+  )
 }
 export default PropertiesPage
