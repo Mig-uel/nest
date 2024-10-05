8abf843a585f65e4f0a187ac37d93ed82cc50d5c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-05 19:13:28 -0400

    Refactor PropertiesPage to fetch properties from the database

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,7 +1,13 @@
+import { connectDB } from '@/config/database'
+import Property from '@/models/property.model'
 import PropertyCard from '@/components/property/property-card.component'
-import properties from '@/properties.json'
+import { IProperty } from '@/types'
 
-const PropertiesPage = () => {
+const PropertiesPage = async () => {
+  await connectDB()
+
+  const properties = (await Property.find({}).lean()) as IProperty[]
+
   return (
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
@@ -10,9 +16,7 @@ const PropertiesPage = () => {
         ) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {properties.map((property) => {
-              const { _id } = property
-
-              return <PropertyCard key={_id} property={property} />
+              return <PropertyCard key={property.name} {...property} />
             })}
           </div>
         )}
