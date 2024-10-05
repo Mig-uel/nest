f993ee6adcbf99f3b5c9eb17c5f7de42d2ad70bc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-05 18:44:47 -0400

    Add types for Property and IUser

---
+++ types.d.ts
@@ -0,0 +1,40 @@
+import { Types } from 'mongoose'
+
+type Property = {
+  _id?: string
+  owner: string
+  name: string
+  type: string
+  description: string
+  location: {
+    street: string
+    city: string
+    state: string
+    zipcode: string
+  }
+  beds: number
+  baths: number
+  square_feet: number
+  amenities: string[]
+  rates: {
+    weekly?: number
+    monthly?: number
+    nightly?: number
+  }
+  seller_info: {
+    name: string
+    email: string
+    phone: string
+  }
+  images: string[]
+  is_featured: boolean
+  createdAt?: string
+  updatedAt?: string
+}
+
+type IUser = {
+  email: string | undefined
+  username: string
+  image: string
+  bookmarks: Types.ObjectId[]
+}
