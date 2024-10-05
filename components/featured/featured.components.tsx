b87c4e4d0054a0d2aa2d43ca363ddb96bd864c2a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-05 00:25:34 -0400

    Add Featured component to display a section of featured properties

---
+++ components/featured/featured.components.tsx
@@ -0,0 +1,68 @@
+import properties from '@/properties.json'
+import PropertyCard from '../property/property-card.component'
+
+type Property = {
+  _id?: string
+  owner: string
+  name: string
+  type: string
+  description: string
+  location: {
+    street: string
+    city: string
+    state: string
+    zipcode: string
+  }
+  beds: number
+  baths: number
+  square_feet: number
+  amenities: string[]
+  rates: {
+    weekly?: number
+    monthly?: number
+    nightly?: number
+  }
+  seller_info: {
+    name: string
+    email: string
+    phone: string
+  }
+  images: string[]
+  is_featured: boolean
+  createdAt?: string
+  updatedAt?: string
+}
+
+const Featured = () => {
+  const randomProperty = () => {
+    const randInt = Math.floor(Math.random() * properties.length)
+
+    return properties[randInt]
+  }
+
+  const featuredProperties = [
+    randomProperty(),
+    randomProperty(),
+    randomProperty(),
+  ]
+
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        <h2 className='text-3xl font-bold text-blue-500 mb-6'>
+          Featured Properties
+        </h2>
+        {!properties.length ? (
+          <p>No properties available</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {featuredProperties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  )
+}
+export default Featured
