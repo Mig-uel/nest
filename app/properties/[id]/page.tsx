b255a8add4b1297b75163b3522aa6e1b4f14dcec
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-05 20:37:47 -0400

    Refactor PropertyPage component to import FaArrowLeft icon

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,11 +3,11 @@ import { connectDB } from '@/config/database'
 import Property from '@/models/property.model'
 
 import PropertyHeaderImage from '@/components/property/property-header-image.component'
+import { FaArrowLeft } from 'react-icons/fa'
 
 import type { Types } from 'mongoose'
 import type { IProperty } from '@/types'
 import Link from 'next/link'
-import { FaArrowLeft } from 'react-icons/fa'
 
 const PropertyPage = async ({
   params: { id },
