a030e6453d7cf07677c8db1232767050bcaabcae
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-07 22:57:49 -0400

    Refactor authOptions to include user creation on sign in

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,5 +1,9 @@
-import { AuthOptions } from 'next-auth'
+import User from '@/models/user.model'
+import { connectDB } from '@/config/database'
 import GoogleProvider from 'next-auth/providers/google'
+
+import type { IUser } from '@/types'
+import type { AuthOptions } from 'next-auth'
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -20,9 +24,25 @@ export const authOptions: AuthOptions = {
     // invoke on successful sign in
     async signIn({ profile }) {
       // connect to database
+      await connectDB()
+
       // check if user exists
-      // create user
+      const userExists = await User.findOne({ email: profile?.email })
+
+      // if not, create user
+      if (!userExists) {
+        // truncate username if too long
+        const username = profile?.name?.slice(0, 20)
+
+        await User.create({
+          email: profile?.email,
+          username,
+          image: profile?.image,
+        })
+      }
+
       // return true to allow sign in
+      return true
     },
 
     // session callback function that modifies the session object
