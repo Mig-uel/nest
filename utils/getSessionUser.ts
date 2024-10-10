066e860ecc986e0f630681922b3aeace76faffd5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-10 03:02:35 -0400

    Refactor getSessionUser to simplify session retrieval

--- utils/getSessionUser.ts
+++ utils/getSessionUser.ts
@@ -2,16 +2,11 @@ import { getServerSession } from 'next-auth/next'
 import { authOptions } from './authOptions'
 
 export const getSessionUser = async () => {
-  try {
-    const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions)
 
-    if (!session || !session.user) return null
+  if (!session || !session.user) return null
 
-    return {
-      user: session.user,
-    }
-  } catch (error) {
-    console.error(error)
-    return null
+  return {
+    user: session.user,
   }
 }
