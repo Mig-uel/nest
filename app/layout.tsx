1ef4e352125784e50b5c4ede5dd08c42ae136d55
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-11 00:42:11 -0400

    Refactor layout.tsx to remove Leaflet dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,6 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <AuthProvider>
       <html>
-        <head>
-          <link
-            rel='stylesheet'
-            href='https://unpkg.com/leaflet@1.9.4/dist/leaflet.css'
-            integrity='sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY='
-            crossOrigin=''
-          />
-          <script
-            src='https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
-            integrity='sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo='
-            crossOrigin=''
-          ></script>
-        </head>
         <body>
           <Navbar />
           <main>{children}</main>
