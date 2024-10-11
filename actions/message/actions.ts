8d61be023d8299a32214fa2755e96cba72f2bbf3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-11 07:44:32 -0400

    Refactor addMessage function to handle sending messages and improve error handling

--- actions/message/actions.ts
+++ actions/message/actions.ts
@@ -1,51 +1,56 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
 import { connectDB } from '@/config/database'
-import User from '@/models/user.model'
 import Message from '@/models/message.model'
-import { HydratedDocument, Types } from 'mongoose'
+import { Types } from 'mongoose'
 import { getSessionUser } from '@/utils/getSessionUser'
 
-import type { IProperty, IUser, IMessage } from '@/types'
+import type { IMessage } from '@/types'
 
-export async function addMessage(formData: FormData) {
-  // session helper function
-  const session = await getSessionUser()
+export async function addMessage(prevState: any, formData: FormData) {
+  try {
+    // session helper function
+    const session = await getSessionUser()
 
-  if (!session || !session.user) throw new Error('User ID is required')
+    if (!session || !session.user) throw new Error('User ID is required')
 
-  // session user id
-  const { id } = session
+    // session user id
+    const { id } = session
 
-  // connect to mongodb
-  await connectDB()
+    // connect to mongodb
+    await connectDB()
 
-  const recipientId = Types.ObjectId.createFromHexString(
-    formData.get('recipient') as string
-  )
-  const propertyId = Types.ObjectId.createFromHexString(
-    formData.get('propertyId') as string
-  )
+    const recipientId = Types.ObjectId.createFromHexString(
+      formData.get('recipientId') as string
+    )
+    const propertyId = Types.ObjectId.createFromHexString(
+      formData.get('propertyId') as string
+    )
 
-  // add this in page instead of backend (i.e. disable or hide messaging feature)
-  if (id === recipientId.toString()) {
-    return { error: 'You cannot send a message to yourself' }
-  }
+    // add this in page instead of backend (i.e. disable or hide messaging feature)
+    if (id === recipientId.toString()) {
+      return { error: 'You cannot send a message to yourself' }
+    }
 
-  const messageObject: IMessage = {
-    sender: Types.ObjectId.createFromHexString(id),
-    recipient: recipientId,
-    property: propertyId,
-    name: formData.get('name') as string,
-    email: formData.get('email') as string,
-    body: formData.get('body') as string,
-    phone: formData.get('phone') as string,
-  }
+    const messageObject: IMessage = {
+      sender: Types.ObjectId.createFromHexString(id),
+      recipient: recipientId,
+      property: propertyId,
+      name: formData.get('name') as string,
+      email: formData.get('email') as string,
+      body: formData.get('body') as string,
+      phone: formData.get('phone') as string,
+    }
 
-  await Message.create(messageObject)
+    await Message.create(messageObject)
 
-  return {
-    message: 'Message sent!',
+    return {
+      message: 'Message sent!',
+      submitted: true,
+    }
+  } catch (error) {
+    return {
+      message: 'Something went wrong',
+    }
   }
 }
