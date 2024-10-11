9d6d33058c827d5757e9e65ce2a153ef7dc6632f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-11 07:44:37 -0400

    Refactor PropertyContactForm to add session user validation and disable form fields when not logged in

--- components/property/property-contact-form.component.tsx
+++ components/property/property-contact-form.component.tsx
@@ -1,12 +1,35 @@
-import { FaPaperPlane } from 'react-icons/fa'
+'use client'
 
+import { useEffect } from 'react'
+import { useFormState, useFormStatus } from 'react-dom'
+import { useSession } from 'next-auth/react'
+import { toast } from 'react-toastify'
+import { addMessage } from '@/actions/message/actions'
+
+import { FaPaperPlane } from 'react-icons/fa'
 import type { IProperty } from '@/types'
 
-const PropertyContactForm = ({}: IProperty) => {
+const PropertyContactForm = ({ _id, owner }: IProperty) => {
+  const { data: session } = useSession()
+
+  const [state, formAction] = useFormState(addMessage, {
+    message: '',
+    submitted: false,
+  })
+
+  const ownerId = owner.toString()
+  const propertyId = _id?.toString()
+
+  if (state.submitted) {
+    return <p className='text-green-500 mb-4'>{state.message}</p>
+  }
+
   return (
     <div className='bg-white p-6 rounded-lg shadow-md'>
       <h3 className='text-xl font-bold mb-6'>Contact Property Manager</h3>
-      <form>
+      <form action={formAction}>
+        <input type='hidden' name='propertyId' defaultValue={propertyId} />
+        <input type='hidden' name='recipientId' defaultValue={ownerId} />
         <div className='mb-4'>
           <label
             className='block text-gray-700 text-sm font-bold mb-2'
@@ -21,6 +44,7 @@ const PropertyContactForm = ({}: IProperty) => {
             type='text'
             placeholder='Enter your name'
             required
+            disabled={!session}
           />
         </div>
         <div className='mb-4'>
@@ -37,6 +61,7 @@ const PropertyContactForm = ({}: IProperty) => {
             type='email'
             placeholder='Enter your email'
             required
+            disabled={!session}
           />
         </div>
         <div className='mb-4'>
@@ -52,6 +77,7 @@ const PropertyContactForm = ({}: IProperty) => {
             name='phone'
             type='text'
             placeholder='Enter your phone number'
+            disabled={!session}
           />
         </div>
         <div className='mb-4'>
@@ -63,15 +89,17 @@ const PropertyContactForm = ({}: IProperty) => {
           </label>
           <textarea
             className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline'
-            id='message'
-            name='message'
+            id='body'
+            name='body'
             placeholder='Enter your message'
+            disabled={!session}
           ></textarea>
         </div>
         <div>
           <button
             className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center'
             type='submit'
+            disabled={!session}
           >
             <FaPaperPlane className='fas fa-paper-plane mr-2' /> Send Message
           </button>
