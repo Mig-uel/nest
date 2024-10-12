f685c61e3ee7e5107b9b92f741d014b1a19ecdcc
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-11 22:28:10 -0400

    Refactor Featured component to limit the number of featured properties displayed

--- components/featured/featured.components.tsx
+++ components/featured/featured.components.tsx
@@ -3,27 +3,34 @@ import Property from '@/models/property.model'
 
 import PropertyCard from '../property/property-card.component'
 import type { IProperty } from '@/types'
+import FeaturedPropertyCard from './featured-card.component'
 
 const Featured = async () => {
   await connectDB()
 
   const featuredProperties = (await Property.find({
     is_featured: true,
-  }).lean()) as IProperty[]
+  })
+    .limit(2)
+    .lean()) as IProperty[]
 
   return (
     <>
-      <section className='px-4 py-6'>
-        <div className='container-xl lg:container m-auto px-4 py-6'>
+      <section className='bg-blue-50 px-4 pt-6 pb-10'>
+        <div className='container-xl lg:container m-auto'>
           <h2 className='text-3xl font-bold text-blue-500 mb-6'>
             Featured Properties
           </h2>
           {!featuredProperties.length ? (
             <p>No featured properties available</p>
           ) : (
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
               {featuredProperties.map((property) => (
-                <PropertyCard key={property.name} {...property} />
+                // <PropertyCard key={property.name} {...property} />
+                <FeaturedPropertyCard
+                  key={property._id?.toString()}
+                  {...property}
+                />
               ))}
             </div>
           )}
