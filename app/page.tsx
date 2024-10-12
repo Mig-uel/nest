3cb991a824dcec2afc9b7e0aeb61c5fe2dbfa26f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-11 22:08:34 -0400

    Refactor HomePage to include Recent component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Featured from '@/components/featured/featured.components'
 import Hero from '@/components/hero/hero.component'
 import InfoBoxes from '@/components/info-boxes/info-boxes.component'
+import Recent from '@/components/recent/recent.properties'
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <InfoBoxes />
 
       <Featured />
+      <Recent />
     </>
   )
 }
